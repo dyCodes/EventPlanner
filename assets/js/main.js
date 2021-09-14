5715a186610eaf7c1e6ee492dea1a142a179d103
dyCodes <yusufdaudu51@gmail.com>
2021-09-14 23:32:11 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*!
+*--------------
+* Template Name: EventPlanner - v1.0
+* Template URL: dycodes.github.com/EventPlanner
+* Author: Yusuf Daudu - dyCodes
+* Author URL: github.com/dycodes
+* License: https://dycodes.github.com/EventPlanner/license
+*---------------------------------
+*/
