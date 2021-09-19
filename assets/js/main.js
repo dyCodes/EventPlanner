9da7cc97971c57c8b2c41a01ca93e5306ebfa25b
dyCodes <yusufdaudu51@gmail.com>
2021-09-19 22:46:13 +0100

    Add header onScroll function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,34 @@
 /*!
-*--------------
-* Template Name: EventPlanner - v1.0
-* Template URL: dycodes.github.com/EventPlanner
-* Author: Yusuf Daudu - dyCodes
-* Author URL: github.com/dycodes
-* License: https://dycodes.github.com/EventPlanner/license
-*---------------------------------
-*/
+ *--------------
+ * Template Name: EventPlanner - v1.0
+ * Template URL: dycodes.github.com/EventPlanner
+ * Author: Yusuf Daudu - dyCodes
+ * Author URL: github.com/dycodes
+ * License: https://dycodes.github.com/EventPlanner/license
+ *---------------------------------
+ */
 
- const galleryLightbox = GLightbox({
-    selector: '.glightbox'
-  });
+const selectElem = (elem, all = false) => {
+	elem = elem.trim();
+	if (all) {
+		return [...document.querySelectorAll(elem)];
+	} else {
+		return document.querySelector(elem);
+	}
+};
+
+window.onscroll = function() {
+	let header = selectElem('#header');
+	let currentPos = window.pageYOffset;
+
+	// Header fixed top on scroll
+	if (currentPos > 50) {
+		header.classList.add('onScroll');
+	} else {
+		header.classList.remove('onScroll');
+	}
+};
+
+const galleryLightbox = GLightbox({
+	selector: '.glightbox'
+});
